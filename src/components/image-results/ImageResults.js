38fe22471959e96e6e4bc0d839c0259fbb55d772
Troy Dorman <troydorman@gmail.com>
2021-07-27 12:32:57 -0600

    imported tons of stuff from material ui into ImageResults

--- src/components/image-results/ImageResults.js
+++ src/components/image-results/ImageResults.js
@@ -1,14 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
-
-
+import { GridList, GridTile } from "material-ui/GridList";
+import IconButton from "material-ui/IconButton";
+import ZoomIn from "material-ui/svg-icons/action/zoom-in";
+import Dialog from "material-ui/Dialog";
+import FlatButton from "material-ui/FlatButton";
 
 const ImageResults = () => {
   return <div></div>;
 };
 
 ImageResults.propTypes = {
-    images: PropTypes.array.isRequired
-}
+  images: PropTypes.array.isRequired,
+};
 
 export default ImageResults;
