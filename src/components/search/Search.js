73d49a53eff5c632e48ae62da061400f9d2ded51
Troy Dorman <troydorman@gmail.com>
2021-07-27 07:44:01 -0600

    created SelectField component and added some Menu Items within

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
+import MenuItem from "material-ui/MenuItem";
 
 const Search = () => {
   const [state, setState] = useState({
@@ -17,6 +18,10 @@ const Search = () => {
     console.log("blahblah");
   };
 
+  const onAmountChange = () => {
+    console.log("blahblah");
+  };
+
   return (
     <div>
       <TextField
@@ -26,6 +31,20 @@ const Search = () => {
         floatingLabelText="Search For Images"
         fullWidth={true}
       />
+      <br />
+      <SelectField
+        name="amount"
+        floatingLabelText="Amount"
+        value={state.amount}
+        onChange={onAmountChange}
+      >
+        <MenuItem value={5} primaryText="5" />
+        <MenuItem value={10} primaryText="10" />
+        <MenuItem value={15} primaryText="15" />
+        <MenuItem value={30} primaryText="30" />
+        <MenuItem value={50} primaryText="50" />
+      </SelectField>
+      <br />
     </div>
   );
 };
