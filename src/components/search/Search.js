ea57c98877225679da5d7b01dc6f7a8db80de803
Troy Dorman <troydorman@gmail.com>
2021-07-27 12:28:49 -0600

    imported ImageResults.  created ternary conditional to generate ImageResults component when state.images > 0

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -3,6 +3,7 @@ import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
 import axios from "axios";
+import ImageResults from "../image-results/ImageResults";
 
 const Search = () => {
   const [state, setState] = useState({
@@ -56,6 +57,7 @@ const Search = () => {
         <MenuItem value={50} primaryText="50" />
       </SelectField>
       <br />
+      {state.images.length > 0 ? <ImageResults images={state.images} /> : null}
     </div>
   );
 };
