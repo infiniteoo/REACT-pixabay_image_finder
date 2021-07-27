40651a2c7c8056669f08f99f790b33cbadc42822
Troy Dorman <troydorman@gmail.com>
2021-07-27 12:31:08 -0600

    created ImageResults component.  imported PropTypes. created proptypes for images made it required

---
+++ src/components/image-results/ImageResults.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+
+const ImageResults = () => {
+  return <div></div>;
+};
+
+ImageResults.propTypes = {
+    images: PropTypes.array.isRequired
+}
+
+export default ImageResults;
